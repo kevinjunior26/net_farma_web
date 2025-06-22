3bc332917869c69cb195fe052f4a0d8dd05b1f68
Kevin Developer <115954924+kjunior-dev@users.noreply.github.com>
2025-06-22 14:02:00 -0100

    fix(product-detail): adjust params type

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,9 +1,9 @@
 export default async function DetalheProduto({
-   params
-} : {
-    params: Promise<{ slug: string }>
+    params,
+}: {
+    params: { slug: string };
 }) {
-    const slug = ( await params).slug
+    const { slug } = params;
     return (
         <div className='container mx-auto px-4 py-8 mt-32 bg-red-400 h-full'>
             <h1>Detalhe do Produto</h1>
